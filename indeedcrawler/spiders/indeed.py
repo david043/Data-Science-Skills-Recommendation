2b4910f9bd22cae333204a34901dc36ddb767bed
Joachim Hagege <cyberjoac@gmail.com>
2018-01-08 11:53:52 +0200

    Cleaning code and preparing for csv

--- indeedcrawler/spiders/indeed.py
+++ indeedcrawler/spiders/indeed.py
@@ -10,7 +10,10 @@ import requests
 from scrapy.http.request import Request
 import time
 
-DOMAIN = 'https://www.indeed.com/jobs?q=data%20scientist&l=United%20States'
+PAGE_NUMBER_TO_CRAWL = 1
+
+BASE_URL = 'https://www.indeed.com/jobs?q=data%20scientist&l=United%20States'
+CHROMEDRIVER_LOC = "/usr/local/bin/chromedriver"
 
 
 # ok, je veux faire un crawler qui, d'abord parse selon toutes les pages
@@ -21,50 +24,42 @@ class IndeedSpider(scrapy.Spider):
     allowed_domains = ['indeed.com']
 
     def __init__(self):
-        dl2 = "/usr/local/bin/chromedriver"
-        self.links = self.get_all_jd()
-        self.start_urls = list(map(lambda x: self.input_in_indeed_string(x), self.links))
-        self.driver = webdriver.Chrome(dl2)
+        self.job_token_ids = self.get_job_token_ids()
+        self.start_urls = [self.full_job_url(job_token_id) for
+                           job_token_id in
+                           self.job_token_ids]
+        self.driver = webdriver.Chrome(CHROMEDRIVER_LOC)
         self.start_requests()
 
     def start_requests(self):
         for url in self.start_urls:
-            print(url)
-            yield Request(url, callback=self.parse_jd)
+            print("Fetching url: " + url)
+            yield Request(url, callback=self.parse_job_description)
 
-    def input_in_indeed_string(self, x):
-        return 'https://www.indeed.com/jobs?q=data%20scientist&l=United%20States&vjk=' + x
+    def full_job_url(self, job_token_id):
+        return BASE_URL + '&vjk=' + job_token_id
 
-    def get_all_jd(self):
+    def get_job_token_ids(self):
+        job_token_ids = []
 
-        my_links = []
-
-        for i in range(1):
-            if i % 10 == 0:
-                print(i)
-            page_url = DOMAIN + '&start=' + str(i * 10)
+        for page_number in range(PAGE_NUMBER_TO_CRAWL):
+            if page_number % 10 == 0:
+                print("Page number: " + str(page_number))
+            starting_job_count = str(page_number * 10)
+            page_url = BASE_URL + '&start=' + starting_job_count
             r = requests.get(page_url)
-            my_links += re.findall("jk:'(\w+)'", r.text)
-        return my_links
+            job_token_ids += re.findall("jk:'(\w+)'", r.text)
+        return job_token_ids
 
-    def parse_jd(self, response):
-
+    def parse_job_description(self, response):
         self.driver.get(response.url)
 
         job_title = self.driver.find_element_by_class_name("jobtitle").text
         company_name = self.driver.find_element_by_class_name("company").text
         summary = self.driver.find_element_by_class_name("summary").text
-        print(job_title + ' , ' + company_name)
-        # with open('job_summaries/jd1.txt', 'w') as file:
-        #     file.write(job_title + "\n")
-        #     file.write(company_name + "\n")
-        #     file.write(summary.encode('ascii', 'ignore').decode('utf-8'))
+
         yield {
-            job_title: job_title,
-            company_name: company_name,
-            summary: summary.encode('ascii', 'ignore').decode('utf-8')
+            'job_title': job_title,
+            'company_name': company_name,
+            'summary': summary.encode('ascii', 'ignore').decode('utf-8')
         }
-        print("FILE WAS WRITTEN")
-
-
-
